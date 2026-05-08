12f4175212ca4e6c1b91acd88c325284e7c23c8d
ilia-jpg <ilia.javan.eng@gmail.com>
2026-05-08 09:43:15 -0400

    initialize main.c with basic includes and main function

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+// device tree
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/spi.h>
+
+
+void main(void) {
+    while(1);
+}
+
