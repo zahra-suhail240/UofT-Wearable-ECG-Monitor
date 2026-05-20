02bb9528b7184f7479e1287eb7b342e23deec15c
ilia-jpg <ilia.javan.eng@gmail.com>
2026-05-20 12:46:59 +0200

    prepare skeleton to do: log message when MAX30001G interrupts

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+// kernel and logging
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
@@ -7,8 +8,21 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/spi.h>
 
+int initialize_MAX();
 
-void main(void) {
-    while(1);
+int main(void) {
+    // TO DO: initialize MAX30001G for interrupts with INTB
+    initialize_MAX();
 }
 
+int initialize_MAX() {
+
+}
+
+// TO DO: configure SPI_read_thread
+
+
+SPI_read_thread() {
+    // TO DO: log message and toggle LED
+
+}
